c507235d7da2383013bdd154d52ebacac2b195ab
shrutimehta2124 <shrutimehta.sarvadhi@gmail.com>
2025-03-04 20:48:52 +0530

    add remaing example of usereduce

---
+++ useReducer/Example9/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useReducer } from 'react';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'toggle':
+      return { isDarkMode: !state.isDarkMode };
+    default:
+      return state;
+  }
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, { isDarkMode: false });
+
+  return (
+    <div style={{ background: state.isDarkMode ? 'black' : 'white', color: state.isDarkMode ? 'white' : 'black' }}>
+      <p>{state.isDarkMode ? 'Dark Mode' : 'Light Mode'}</p>
+      <button onClick={() => dispatch({ type: 'toggle' })}>Toggle Mode</button>
+    </div>
+  );
+}
+export default App;
